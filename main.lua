673c385b4edd8ed91873e578af203aab1884d449
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-10-11 19:05:17 +0200

    Don't worry about hidden items

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
--- Item Sorter v1.0.4
+-- Item Sorter v1.0.5
 -- SmoothSpatula
 
 log.info("Successfully loaded ".._ENV["!guid"]..".")
-mods.on_all_mods_loaded(function() for k, v in pairs(mods) do if type(v) == "table" and v.hfuncs then Helper = v end end end)
+mods.on_all_mods_loaded(function() for _, m in pairs(mods) do if type(m) == "table" and m.RoRR_Modding_Toolkit then for _, c in ipairs(m.Classes) do if m[c] then _G[c] = m[c] end end end end end)
 mods.on_all_mods_loaded(function() for k, v in pairs(mods) do if type(v) == "table" and v.tomlfuncs then Toml = v end end 
     params = {
         item_sorter_enabled = true
@@ -22,17 +22,6 @@ gui.add_to_menu_bar(function()
     end
 end)
 
--- ========== Utils ==========
-
-function is_player(actor)
-    local players = Helper.find_active_instance_all(gm.constants.oP)
-    for _, player in ipairs(players) do
-        if player.id == actor.id then return true end
-    end
-
-    return false
-end
-
 -- ========== Main ==========
 
 gm.post_script_hook(gm.constants.item_give_internal, function(self, other, result, args)
@@ -42,26 +31,27 @@ gm.post_script_hook(gm.constants.item_give_internal, function(self, other, resul
     local item_id = args[2].value
     local amount = args[3].value
     local item_index = #actor.inventory_item_order-1
+    local incoming_item = Item.wrap(item_id)
 
-    if not is_player(actor) or #actor.inventory_item_order == 1 or item_id == 132 then return end
+    print(incoming_item.identifier)
+    print(incoming_item.tier)
+
+    if actor.object_name ~= "oP" or #actor.inventory_item_order == 1 or incoming_item.is_hidden==true then return end
 
     local incoming_amount = actor.inventory_item_stack[item_id+1]
-
     if incoming_amount ~= amount then
         item_index = gm.array_get_index(actor.inventory_item_order, item_id)
     end
-
     gm.array_delete(actor.inventory_item_order, item_index, 1)
 
-    local incoming_item = gm.variable_global_get("class_item")[item_id+1]
     
     for i = #actor.inventory_item_order, 1, -1 do
         local inventory_item = gm.variable_global_get("class_item")[actor.inventory_item_order[i]+1]
 
         local inventory_amount = actor.inventory_item_stack[actor.inventory_item_order[i]+1]
         
-        if incoming_item[7] < inventory_item[7] 
-        or (incoming_item[7] == inventory_item[7] and incoming_amount <= inventory_amount) then
+        if incoming_item.tier < inventory_item[7] 
+        or (incoming_item.tier == inventory_item[7] and incoming_amount <= inventory_amount) then
             gm.array_insert(actor.inventory_item_order, i, item_id)
             return
         end
